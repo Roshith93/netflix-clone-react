25634f0f5434bb9054dee94a425dca3e804f71d2
Roshith R <r.roshith@gmail.com>
2020-12-09 18:18:18 +0530

    Added the container and applied global styles

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,25 +1,33 @@
-import { Inner, Container, Title, SubTitle, Pane, Image, Item } from './styles/jumbotron'
+import {
+  Inner,
+  Container,
+  Title,
+  SubTitle,
+  Pane,
+  Image,
+  Item,
+} from './styles/jumbotron'
 
-export const Jumbotron = ({ direction = 'row', children, ...restProps }) => <Item direction={direction}>
- <Inner>
-  {children}
- </Inner>
-</Item>
+export const Jumbotron = ({ direction = 'row', children, ...restProps }) => (
+  <Item {...restProps}>
+    <Inner direction={direction}>{children}</Inner>
+  </Item>
+)
 
 const JumbotronContainer = ({ children, ...restProps }) => {
- return <Container {...restProps}>{children}</Container>
+  return <Container {...restProps}>{children}</Container>
 }
 const JumbotronTitle = ({ children, ...restProps }) => {
- return <Title {...restProps}>{children}</Title>
+  return <Title {...restProps}>{children}</Title>
 }
 const JumbotronSubTitle = ({ children, ...restProps }) => {
- return <SubTitle {...restProps}>{children}</SubTitle>
+  return <SubTitle {...restProps}>{children}</SubTitle>
 }
 const JumbotronPane = ({ children, ...restProps }) => {
- return <Pane {...restProps}>{children}</Pane>
+  return <Pane {...restProps}>{children}</Pane>
 }
 const JumbotronImage = ({ ...restProps }) => {
- return <Image {...restProps} />
+  return <Image {...restProps} />
 }
 Jumbotron.Container = JumbotronContainer
 Jumbotron.Title = JumbotronTitle
