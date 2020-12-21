e85f17e5a43bfdddcc2bde874b2b9fc31dd692d3
Roshith R <r.roshith@gmail.com>
2020-12-21 16:01:08 +0530

    accessed films and series in browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,4 +1,8 @@
+import {UseContent} from '../hooks/UseContent'
+
 export const Browse = () => {
+ const {series} = UseContent('series')
+ const {films} = UseContent('films')
  return(
   <p>from Browse </p>
  )
