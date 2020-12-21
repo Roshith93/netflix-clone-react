a26b9d645282fbd78ba9962f8e457e9d17fb500a
Roshith R <r.roshith@gmail.com>
2020-12-21 16:35:54 +0530

    categorized the data by genre

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,8 +1,10 @@
 import {UseContent} from '../hooks/UseContent'
-
+import {SelectionTitle} from '../utils/SelectionTitle'
 export const Browse = () => {
  const {series} = UseContent('series')
  const {films} = UseContent('films')
+ const slides = SelectionTitle({series, films})
+ console.log(slides)
  return(
   <p>from Browse </p>
  )
